9e507735b0e848417a3330348ec06d984501e7c9
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2024-06-15 11:39:29 +0200

    wip

--- src/classes/constants.ts
+++ src/classes/constants.ts
@@ -1,5 +1,29 @@
 export enum DBConstants {
-    databaseName = "vhs",
-    userIdField = 'userId',
-    baseSaveGameId = 'base'
+  databaseName = "vhs",
+  userIdField = "userId",
+  baseSaveGameId = "base",
+}
+
+export enum EDiscoveryDataType {
+  DDT_AllInventoryItemsBit = 0,
+  DDT_AllLoadoutsBit = 1,
+  DDT_AllSceneEnactmentStatesBit = 2,
+  DDT_AllPlayerAccountPointsBit = 3,
+  DDT_AllPlayerSlotsBit = 4,
+  DDT_AllFriendListsBit = 5,
+  DDT_AllUnclaimedChestsBit = 6,
+  DDT_AllStoreItemsBit = 7,
+  DDT_SpecificLoadoutsBit = 8,
+  DDT_QuestSystemBit = 9,
+  DDT_SeasonPassBit = 10,
+  DDT_AllWeaponsBit = 11,
+  DDT_JourneyDataBit = 12,
+  DDT_AccountStatsBit = 13,
+  DDT_GuideSystemBit = 14,
+  DDT_EntitlementsBit = 15,
+  DDT_PatchNotesBit = 16,
+  DDT_SeasonalEventBit = 17,
+  DDT_ServerNotificationBit = 18,
+  DDT_DynamicBountyRewardsBit = 19,
+  DDT_CommunityGoalsBit = 20,
 }
