5008f3da3ba1951ce819153f7441de9bfaab65f3
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2024-11-03 21:02:12 +0100

    wip

--- src/websocket-server/types.ts
+++ src/websocket-server/types.ts
@@ -1,27 +1,66 @@
 /* Request is used for any Client-> Server communication
  while response is used for any Server-Client communication, independently of who is actually starting it
 */
-export interface WSRequest {
-    action: '' | 'OnHeartbeat' | 'OnLogin';
+export type WSRequest =
+  | LoginRequest
+  | QueueRequest
+  | HeartBeatRequest
+  | { action: "" };
+
+export interface HeartBeatRequest {
+  action: "OnHeartbeat";
 }
 
-export interface HeartBeatRequest extends WSRequest {
-    action: 'OnHeartbeat';
+export interface LoginRequest {
+  action: "OnLogin";
+  gameClientGuid: string;
+  clientBuildNumber: `${number}`;
+  sessionTicketId: string;
+  playerAccountId: string;
+  cloudScriptVersion: number;
 }
 
-export interface LoginRequest extends WSRequest {
-    action: "OnLogin",
-    gameClientGuid: string,
-    clientBuildNumber: `${number}`,
-    sessionTicketId: string,
-    playerAccountId: string,
-    cloudScriptVersion: number
+export interface QueueRequest {
+  action: "OnQueueAs";
+  readonly matchmakingStatus: string;
+  readonly matchmakingRegion: string;
+  readonly gameClientGuid: string;
+  readonly playerAccountId: string;
+  readonly displayName: string;
+  readonly preferredFaction: string;
+  readonly partyId: string;
+  readonly partyMemberPlayerAccountIds: number[];
+  readonly characterType: string;
+  readonly matchmakingTicketId: string;
+  readonly matchId: string;
+  readonly dsIpAddress: string;
+  readonly dsPort: number;
 }
 
-export interface WSResponse {
-    action: 'Heartbeat',
-    result: boolean,
-    backendError: number, 
-    extraMessage: string,
-    data: string
+export type WSResponse = HeartbeatResponse | MatchmagingNotification;
+export interface HeartbeatResponse {
+  action: "Heartbeat";
+  result: boolean;
+  backendError: number;
+  extraMessage: string;
+  data: string;
+}
+
+export interface MatchmagingNotification {
+  readonly action: "MatchmakingNotification";
+  readonly result: boolean;
+  readonly backendError: null;
+  readonly extraMessage: string;
+  readonly data: Data;
+}
+
+export interface Data {
+  readonly matchmakingType: 'MatchmakingSearching';
+  readonly serverIp: string;
+  readonly serverPort: number;
+  readonly players: Player[];
+}
+
+export interface Player {
+  readonly playerAccountId: string;
 }
--- src/websocket-server/websocket-server.ts
+++ src/websocket-server/websocket-server.ts
@@ -1,53 +1,77 @@
-import { WebSocket, WebSocketServer } from 'ws';
-import { HeartBeatRequest, LoginRequest, WSRequest, WSResponse } from './types';
-import { Handler } from '../classes/handler';
-import { Logger } from '../classes/logger';
+import { WebSocket, WebSocketServer } from "ws";
+import {
+  HeartBeatRequest,
+  HeartbeatResponse,
+  LoginRequest,
+  MatchmagingNotification,
+  WSRequest,
+  WSResponse,
+} from "./types";
+import { Handler } from "../classes/handler";
+import { Logger } from "../classes/logger";
 
 const wss = new WebSocketServer({ port: 12479 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+wss.on("connection", function connection(ws) {
+  let id: string;
+  ws.on("error", console.error);
   ws.on("close", () => WSClientManager.removeClient(ws));
 
-  ws.on('message', function message(data) {
-    const msg = data.toString('utf-8');
-    let parsedMsg: WSRequest = {action: ''};
-    try{
+  ws.on("message", function message(data) {
+    const msg = data.toString("utf-8");
+    let parsedMsg: WSRequest = { action: "" };
+    try {
       parsedMsg = JSON.parse(msg);
     } catch (e) {
       console.error(e);
     }
 
     switch (parsedMsg.action) {
-      case 'OnLogin':
-        const typedMsg = parsedMsg as LoginRequest;
-        WSClientManager.addNewClient(ws, Handler.getId(typedMsg.sessionTicketId));
+      case "OnLogin":
+        id = Handler.getId(parsedMsg.sessionTicketId);
+        WSClientManager.addNewClient(ws, id);
         break;
-      case 'OnHeartbeat':
-        const obj: WSResponse = {
-          action: 'Heartbeat',
+      case "OnHeartbeat":
+        const hbResponse: HeartbeatResponse = {
+          action: "Heartbeat",
           backendError: 0,
-          data: '',
-          extraMessage: '',
-          result: true
+          data: "",
+          extraMessage: "",
+          result: true,
         };
-        ws.send(JSON.stringify(obj));
+        ws.send(JSON.stringify(hbResponse));
+        break;
+      case "OnQueueAs":
+        // TODO DISCORD INTEGRATION AND NAME SAVING LOGIC
+        const matchMakingNotification: MatchmagingNotification = {
+          action: "MatchmakingNotification",
+          backendError: null,
+          result: true,
+          extraMessage: "",
+          data: {
+            matchmakingType: "MatchmakingSearching",
+            players: [{ playerAccountId: id }],
+            serverIp: "",
+            serverPort: 0,
+          },
+        };
+        ws.send(JSON.stringify(matchMakingNotification));
+        break;
       default:
-        Logger.log('Unknown request', msg);
+        Logger.log("Unknown request", msg);
     }
   });
 });
 
-
 export class WSClientManager {
-    private static clients: {ws: WebSocket, userId: string}[] = [];
+  private static clients: { ws: WebSocket; userId: string }[] = [];
 
-    public static addNewClient(ws: WebSocket, userId: string) {
-        WSClientManager.clients.push({ws, userId});
-    }
+  public static addNewClient(ws: WebSocket, userId: string) {
+    WSClientManager.clients.push({ ws, userId });
+  }
 
-    public static removeClient(ws: WebSocket) {
-        const idx = WSClientManager.clients.findIndex(client => client.ws === ws);
-        WSClientManager.clients.splice(idx, 1);
-    }
+  public static removeClient(ws: WebSocket) {
+    const idx = WSClientManager.clients.findIndex((client) => client.ws === ws);
+    WSClientManager.clients.splice(idx, 1);
+  }
 }
